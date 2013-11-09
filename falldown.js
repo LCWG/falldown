cb8d70db221ce810cce6c34abead3cff46647dc6
Eliot Piering <efpierin@wustl.edu>
2013-11-09 13:29:18 -0600

    added the animation shim and broke some other stuff

---
+++ falldown.js
@@ -0,0 +1,131 @@
+    window.requestAnimFrame = (function(callback) {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame  ||
+        window.mozRequestAnimationFrame     ||
+        function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+var canvas, context, toggle;
+
+init();
+animate();
+
+function init() {
+    canvas = document.getElementById("falldown")
+    context = canvas.getContext( '2d' );
+
+}
+
+function animate() {
+    requestAnimFrame( animate );
+    draw();
+
+}
+
+function draw() {
+    var key; //for the which key was pressed
+    var count;
+    var acceleration = 1; //the speed the ball changes
+    var ballX; //Starting X position
+    var ballY;  //Starting Y position
+    var ballDy = -1;
+    var boardX = 600;  //game board width
+    var boardY = 600;  //game board height
+    var lineArray; //set the potential line to empty array;
+    var newLine = [];
+
+    function makeBoard(ballX, ballY, newLine, lineArray) {
+        ///call this only once per loop to draw the canvas
+
+        //the board
+        context.clearRect(0, 0, boardX, boardY);
+        context.fillStyle = "lightGrey";
+        context.beginPath();
+        context.rect(0, 0, boardX, boardY);
+        context.closePath();
+        context.fill();
+
+        //the ball
+        context.fillStyle = "black";
+        context.beginPath();
+        context.arc(ballX, ballY, 5, 0, Math.PI * 2, true);
+        context.closePath();
+        context.fill();
+
+        //draw the line
+        for (var i=0; i< newLine.length; i++) {
+            if (line[i] == 0 ) {
+                //then don't draw anything
+            } else {
+                context.fillStyle = "black"
+                context.beginPath();
+                context.rect(0, 0, 0, 0);
+
+
+            }
+        };
+    };
+
+    ///jquery get the keypress
+    jQuery(document).on('keypress', function(e){
+
+        switch (e.which) {
+            // Left arrow.
+            case 37:
+                //set global variable key to left
+                key = 'left'
+                break;
+
+            // Right arrow
+            case 39:
+                //set global variable key to right
+                key = 'right'
+                break;
+
+        };
+    });
+
+    //if ballX and ballY haven't been set, set them
+    if (!(ballX || ballY)) {
+        ballX = 300;
+        ballY = 300;
+    }
+
+    //change the x position  of the ball according to key press
+    if (ballX > 0 && ballX < boardX) {
+    //if it was left
+        if (key == 'left') {
+            ballX = ballX - acceleration;
+            //if it was right
+        } else if (key == 'right'){
+                ballX = ballX + acceleration;
+            };
+        } else if (ballX <= 0) {
+            ballX=acceleration;
+        }
+        else if  (ballX >= boardX) {
+            ballX = boardX-acceleration;
+        };
+
+    //change the y position of the ball
+    ballY = ballY + ballDy;
+
+    //create an object literal that has function to create and add new lines
+    function createLine() {
+        var line;
+        for (var i=0; i<20; i++){
+            random = Math.random()
+            //create a array with 1 and 0's 0's will be holes  1's line
+            if (random < 0.85) {
+                line.push(1);
+            }else {
+                line.push(0);
+            };
+        };
+        return line
+    }
+
+    //call this at the end to draw the board
+    makeBoard(ballX, ballY, newLine, lineArray);
+};
