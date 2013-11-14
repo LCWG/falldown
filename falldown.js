c52bf79552b13721b7eb540aa8293c337f63c621
Eliot Piering <efpierin@wustl.edu>
2013-11-13 22:48:12 -0600

    added lines and the logic to move through lines

--- falldown.js
+++ falldown.js
@@ -6,196 +6,133 @@
             window.setTimeout(callback, 1000 / 60);
         };
 })();
-var canvas, context, toggle;
 
-<<<<<<< HEAD
+function newGame () {
 
-    animate();
-=======
-init();
-animate();
->>>>>>> parent of 16bac91... wip
-
-function init() {
-    canvas = document.getElementById("falldown")
-    context = canvas.getContext( '2d' );
-
-}
-
-function animate() {
-    requestAnimFrame( animate );
-    draw();
-
-}
-
-function draw() {
-<<<<<<< HEAD
-    var first;
-
-    if (first == undefined) {
-        var ball = new Ball();
-        var board = new Board();
-        var key = false
-        var canvas = document.getElementById("falldown")
-        var context = canvas.getContext( '2d' );
-        first = false;
-    }
-
-    if (ball == undefined) {
-        ball = new Ball();
-    }
-    if (board == undefined) {
-        board = new Board();
-    }
-=======
+   //initialize variables
+    var canvas = document.getElementById("falldown");
+    var context = canvas.getContext( '2d' );
+    var ball = new Ball();
+    var board = new Board();
+    var line = new Line();
+    line.makeLine();
     var key; //for the which key was pressed
-    var count;
-    var acceleration = 1; //the speed the ball changes
-    var ballX; //Starting X position
-    var ballY;  //Starting Y position
-    var ballDy = -1;
-    var boardX = 600;  //game board width
-    var boardY = 600;  //game board height
-    var lineArray; //set the potential line to empty array;
-    var newLine = [];
-
-    function makeBoard(ballX, ballY, newLine, lineArray) {
-        ///call this only once per loop to draw the canvas
-
-        //the board
-        context.clearRect(0, 0, boardX, boardY);
-        context.fillStyle = "lightGrey";
-        context.beginPath();
-        context.rect(0, 0, boardX, boardY);
-        context.closePath();
-        context.fill();
-
-        //the ball
-        context.fillStyle = "black";
-        context.beginPath();
-        context.arc(ballX, ballY, 5, 0, Math.PI * 2, true);
-        context.closePath();
-        context.fill();
-
-        //draw the line
-        for (var i=0; i< newLine.length; i++) {
-            if (line[i] == 0 ) {
-                //then don't draw anything
-            } else {
-                context.fillStyle = "black"
-                context.beginPath();
-                context.rect(0, 0, 0, 0);
+    var acceleration = 6; //the speed the ball changes
+    var fallDownSpeed = 0.25
+    var count = 0;
+    var gameOver = false;
 
 
-            }
-        };
-    };
+    //start the loop
+    animate();
 
 
-    ///jquery get the keypress
-    jQuery(document).on('keypress', function(e){
-
-        switch (e.which) {
-            // Left arrow.
-            case 37:
-                //set global variable key to left
-                key = 'left'
-                break;
-
-            // Right arrow
-            case 39:
-                //set global variable key to right
-                key = 'right'
-                break;
-
-        };
-    });
-
-    //if ballX and ballY haven't been set, set them
-    if (!(ballX || ballY)) {
-        ballX = 300;
-        ballY = 300;
-    }
-
-    //change the x position  of the ball according to key press
-    if (ballX > 0 && ballX < boardX) {
-    //if it was left
-        if (key == 'left') {
-            ballX = ballX - acceleration;
-            //if it was right
-        } else if (key == 'right'){
-                ballX = ballX + acceleration;
-            };
-        } else if (ballX <= 0) {
-            ballX=acceleration;
-        }
-        else if  (ballX >= boardX) {
-            ballX = boardX-acceleration;
-        };
-
-    //change the y position of the ball
-<<<<<<< HEAD
-    ball.y = ball.y + 1;
-
-    //draw the ball
-    context.fillStyle = "black";
-    context.beginPath();
-    context.arc(ball.x, ball.y, 5, 0, Math.PI * 2, true);
-    context.closePath();
-    context.fill();
-
-    //draw the board
-    context.fillStyle = "lightGrey";
-    context.beginPath();
-    context.rect(0, 0, board.x, board.y);
-    context.closePath();
-    context.fill();
-
-    ///the objects
+    ///my Objects
     function Ball () {
         this.x = 300;
         this.y = 300;
+        this.draw = function (x, y) {
+            context.fillStyle = "black";
+            context.beginPath();
+            context.arc(x, y, 5, 0, Math.PI * 2, true);
+            context.closePath();
+            context.fill();
+        }
     }
 
     function Board () {
-        this.x = 600;
-        this.y = 600;
+        this.width = 600;
+        this.height = 600;
+        this.draw = function (width, height) {
+            context.fillStyle = "lightGrey";
+            context.beginPath();
+            context.rect(0, 0, width, height);
+            context.closePath();
+            context.fill();
+        }
     }
 
-    function line() {
+    function Line() {
+        this.y = 350;
+        this.line = [];
+        this.hole = null;
+        this.space = 200;
         this.draw = function () {
-            var line;
-            for (var i=0; i<20; i++){
-                random = Math.random()
-                //create a array with 1 and 0's 0's will be holes  1's line
-                if (random < 0.85) {
-                    line.push(1);
-                }else {
-                    line.push(0);
-                };
-=======
-    ballY = ballY + ballDy;
+            context.beginPath();
+            context.moveTo(0,this.y);
+            context.lineTo(this.line[0], this.y);
+            context.lineWidth = 5;
+            context.strokeStyle = '#ff0000';
+            context.stroke();
 
-    //create an object literal that has function to create and add new lines
-    function createLine() {
-        var line;
-        for (var i=0; i<20; i++){
-            random = Math.random()
-            //create a array with 1 and 0's 0's will be holes  1's line
-            if (random < 0.85) {
-                line.push(1);
-            }else {
-                line.push(0);
->>>>>>> parent of 16bac91... wip
-            };
+            context.beginPath();
+            context.moveTo(this.line[1],this.y);
+            context.lineTo(600, this.y);
+            context.lineWidth = 5;
+            context.strokeStyle = '#ff0000';
+            context.stroke();
         };
-        return line
+
+        this.makeLine = function () {
+            var randomHole = Math.round(Math.random()*600);
+            this.line = [randomHole-this.space, randomHole];
+            this.hole = randomHole
+        }
     }
 
-<<<<<<< HEAD
+    function animate() {
+        if (!gameOver) {
+            requestAnimFrame( animate );
+            draw();
+        } else {
+            newGame();
+        }
+    }
 
-=======
-    //call this at the end to draw the board
-    makeBoard(ballX, ballY, newLine, lineArray);
->>>>>>> parent of 16bac91... wip
-};
+        function draw() {
+            //change the x position  of the ball according to key press
+            if (ball.x > 0 && ball.x < board.width) {
+            //if it was left
+                if (key == 'Left') {
+                    ball.x = ball.x - acceleration;
+                    //if it was right
+                } else if (key == 'Right'){
+                    ball.x = ball.x + acceleration;
+                };
+                } else if (ball.x <= 0) {
+                ball.x=acceleration;
+                }
+                else if  (ball.x >= board.width) {
+                ball.x = board.width-acceleration;
+                };
+
+            //change the y position of the ball
+            if (ball.y >= line.y - 7 ) {
+                if ((line.hole > ball.x) && (ball.x > line.hole - line.space)) {
+                    ball.y = ball.y + fallDownSpeed;
+                }
+            } else {
+                ball.y = ball.y + fallDownSpeed;
+            }
+
+            ///draw everything
+            board.draw(board.width, board.height);
+            line.draw();
+            ball.draw(ball.x, ball.y);
+            count += 1;
+
+        }
+
+
+    ///jquery respods to the keypress
+    jQuery(document).keypress(function(e){
+
+        key = e.key
+        return key
+
+        });
+
+}
+
+
+newGame();
